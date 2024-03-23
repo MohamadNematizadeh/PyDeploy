d6dc99870666d0159c56be483d63f4b3421788b4
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-03-23 19:13:24 +0330

    Update main.py

--- 1.3.Hello_FastAPI/Solar_System/main.py
+++ 1.3.Hello_FastAPI/Solar_System/main.py
@@ -4,28 +4,28 @@ from urllib.request import urlretrieve
 from fastapi.responses import FileResponse
 
 app = FastAPI()
-plants = plants = json.load(open("jsons/planet.json"))
+planet = planet = json.load(open("jsons/planet.json"))
 
    
 @app.get("/")
 def read_root():
-    text =  "Hi, welcome to the solar system api In this api there is about solar system plants"
+    text =  "Hi, welcome to the solar system api In this api there is about solar system planet"
     return text
             
 
 @app.get("/planet")
-def get_plants():
-    return plants
+def get_planet():
+    return planet
     
 
-@app.get("/planet/{name_plants}/")
-def test2(name_plants: str):
-    return plants[name_plants]
+@app.get("/planet/{name_planet}/")
+def test2(name_planet: str):
+    return planet[name_planet]
 
 
-@app.get("/planet/{name_plants}/image")
-def create_image(name_plants: str):
-    url_images= plants[name_plants]
+@app.get("/planet/{name_planet}/image")
+def create_image(name_planet: str):
+    url_images= planet[name_planet]
     url_image = url_images["image"]
     image = urlretrieve(url_image, "local-plant.png")
     return FileResponse("local-plant.png")
