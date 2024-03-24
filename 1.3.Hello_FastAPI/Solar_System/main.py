044067ef9b0776e5461b14b0a71b1039d4e255d1
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-03-24 09:25:47 +0330

    Update main.py

--- 1.3.Hello_FastAPI/Solar_System/main.py
+++ 1.3.Hello_FastAPI/Solar_System/main.py
@@ -1,31 +1,37 @@
+import io
 import json
+import cv2
 from fastapi import FastAPI 
 from urllib.request import urlretrieve
 from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse 
+
 
 app = FastAPI()
-planet = json.load(open("jsons/planet.json"))
+plants = plants = json.load(open("jsons/planet.json"))
 
    
 @app.get("/")
-def start():
-    text =  "Hi, welcome to the solar system api In this api there is about solar system planet"
+def read_root():
+    text =  "Hi, welcome to the solar system api In this api there is about solar system plants"
     return text
             
 
 @app.get("/planet")
-def get_planet():
-    return planet
+def get_plants():
+    return plants
     
 
-@app.get("/planet/{name_planet}/")
-def get_planet_name(name_planet: str):
-    return planet[name_planet]
+@app.get("/planet/{name_plants}/")
+def test2(name_plants: str):
+    return plants[name_plants]
 
 
-@app.get("/planet/{name_planet}/image")
-def create_planet_image(name_planet: str):
-    url_images= planet[name_planet]
+@app.get("/planet/{name_plants}/image")
+def create_image(name_plants: str):
+    url_images= plants[name_plants]
     url_image = url_images["image"]
-    image = urlretrieve(url_image, "local-plant.png")
-    return FileResponse("local-plant.png")
+    image2 = urlretrieve(url_image, "local-plant.png")
+    image = cv2.imread("local-plant.png")
+    _, encode_image = cv2.imencode(".png", image)
+    return StreamingResponse(content=io.BytesIO(encode_image.tobytes()), media_type = "image/png")
