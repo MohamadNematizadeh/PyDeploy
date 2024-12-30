0f933501feb06675e0f1464d9707aece65c86e0a
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-12-30 19:56:14 +0330

    Delete 9.Django directory

--- 9.Django/9.1.Hello_Django/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index ,name ="index"),
-    
-]
