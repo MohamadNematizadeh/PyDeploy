c86aded337a95dff91d183c91b01f246dc6d9832
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2025-01-04 20:07:09 +0330

    Update views.py

--- 9.Django/polls/views.py
+++ 9.Django/polls/views.py
@@ -25,30 +25,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = "results.html"
     
-    
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "detail.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
