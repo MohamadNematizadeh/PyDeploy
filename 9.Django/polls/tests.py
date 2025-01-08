46ff50d072e8f305e7dfc41ba059d812de093eea
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2025-01-08 15:09:56 +0330

    Update tests.py

--- 9.Django/polls/tests.py
+++ 9.Django/polls/tests.py
@@ -1,3 +1,69 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Question
 
-# Create your tests here.
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is in the future.
+        """
+        future_question = create_question("Future question", 30)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is older than 1 day.
+        """
+        old_question = create_question("Old question", -2)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date is within the last day.
+        """
+        recent_question = create_question("Recent question", -0.5)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "هیچ سوالی برای نمایش موجود نیست.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the index page.
+        """
+        question = create_question("Past question", -30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [question])
+
+   
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on the index page.
+        """
+        create_question("Future question", 30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "هیچ سوالی برای نمایش موجود نیست.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions are displayed.
+        """
+        past_question = create_question("Past question", -30)
+        create_question("Future question", 30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [past_question])
